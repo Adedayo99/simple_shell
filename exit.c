e4adb28e8b1dacc23a332338cf49f2443a61c204
Sirmurhel <samuelzee256@gmail.com>
2022-12-21 04:27:27 -0800

    implementing exit functionality

---
+++ exit.c
@@ -0,0 +1,24 @@
+/**
+* exit - terminates the shell program
+*@status: return value to parent
+*
+* Return: void
+*/
+#include "shell.h"
+
+void exit(int status)
+{
+
+	fflush(stdin);
+	fflush(NULL);
+
+	for (i = 0; i < 4; i++)
+	close(i);
+
+	if (status == 1000)
+	_exit;
+
+	else
+	_exit(status);
+
+}
